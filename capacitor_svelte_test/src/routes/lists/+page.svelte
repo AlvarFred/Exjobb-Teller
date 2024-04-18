0185f00f014da536b611273fdc13f72edb50d00f
AlvarFred <alvarf2016@outlook.com>
2024-04-18 10:13:58 +0200

    List desing using cards

--- capacitor_svelte_test/src/routes/lists/+page.svelte
+++ capacitor_svelte_test/src/routes/lists/+page.svelte
@@ -1,19 +1,21 @@
 <script>
 	import { Block, BlockTitle, Card, Navbar, NavbarBackLink, Page } from "konsta/svelte";
-    import NatureImg1 from '$lib/assets/nature.jpg'
-    import NatureImg2 from '$lib/assets/nature2.jpeg'
+
     const lists = [{
         name: "Self love",
-        image: NatureImg1
+        completed: 2
     },{
         name: "I'm the best",
-        image: NatureImg2
+        completed: 0
+
     },{
         name: "My own Boss",
-        image: NatureImg1
+        completed: 72
+
     },{
         name: "I am the one",
-        image: NatureImg2
+        completed: 8
+
     },]
 
 
@@ -24,21 +26,12 @@
 		<NavbarBackLink slot="left" text="Back" onClick={() => history.back()} />
 	</Navbar>
     <BlockTitle large class="text-black mt-[0px] mx-[10px]">Affirmation lists</BlockTitle>
-    <Block class="grid grid-cols-2">
+    <Block class="">
         {#each lists as list}
-                <div class="drop-shadow-xl flex justify-center items-center m-[10px] rounded-lg text-center h-[20vh]" style="background-image: url({list.image}); background-size: cover; background-repeat: no-repeat;">
-                    
-                    <!-- <img class="h-[50%] " alt="Preview for lists" src={list.image}/> -->
-                    
-                    <p class="text-white text-2xl text-shadow">{list.name}</p>
-                </div>
+               <Card raised>
+                <h2 class="text-lg">{list.name}</h2>
+                <p class="text-xs">Completed {list.completed} times</p>
+               </Card>
         {/each } 
     </Block>
 </Page>
-
-<style>
-    .text-shadow{
-		/* offset-x | offset-y | blur-radius | color */
-		text-shadow: 0px 0px 4px black;
-	}
-</style>
