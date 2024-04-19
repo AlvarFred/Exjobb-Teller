3df9700224d34033f5a3bff92d442a01cb240221
Gustav Dahl Deurell <gusda481@student.liu.se>
2024-04-19 12:50:41 +0200

    Audio from local file can be played in app

--- capacitor_svelte_test/src/routes/ingame/+page.svelte
+++ capacitor_svelte_test/src/routes/ingame/+page.svelte
@@ -6,6 +6,7 @@
 	import {wordErrorRate} from '$lib/wer.js';
 	import nature from '$lib/assets/nature.jpg';
 	import nature2 from '$lib/assets/nature2.jpeg';
+    import Fail_Sound from '$lib/audio/Fail_Sound.wav'
 
 	import {
 		Page,
@@ -20,6 +21,7 @@
 		BlockFooter
 	} from 'konsta/svelte';
 	import {logData} from '$lib/logData.js';
+	import { Capacitor } from '@capacitor/core';
 
 	const dummyData = [
 		{
@@ -33,6 +35,9 @@
 		},
 		{ affirmation: 'The third affirmation, this one is really hard', img: nature }
 	];
+
+    const failAudio = new Audio(Fail_Sound);
+    const successAudio = new Audio();
 
 	const allowedWER = 0.3;
 	let speechInput = '';
@@ -83,16 +88,19 @@
 			
             console.log("###################", jsonData)
             logData(jsonData)
-
+            
 			if (wer > allowedWER) {
-				// Fail
+                // Fail
 				console.log('Failed: WER = ', wer, ' > allowed = ', allowedWER);
 				// TODO: Show user that speech recognition failed
+                console.log("##### PLAY FAIL AUDIO")
+                failAudio.play();
 			} else {
 				// Success
 				console.log('Success: WER = ', wer, ' < allowed = ', allowedWER);
 				// TODO: Do some cool animation to show user speech recognition succeeded
 				sentenceComplete = true
+                successAudio.play()
 				//step();
 			}
 		} catch(e){
